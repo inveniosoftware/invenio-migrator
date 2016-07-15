4b4b2d4f954c3610f1dd94e0caae30f37f3b7a24
Krzysztof Nowak <k.nowak@cern.ch>
2016-07-15 18:34:35 +0200

    cli: loading cli refactoring
    * Data loading CLI now accepts multiple 'source' arguments.
    
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- invenio_migrator/cli.py
+++ invenio_migrator/cli.py
@@ -43,157 +43,162 @@ def dumps():
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), nargs=-1)
 @click.option('--source-type', '-t',  type=click.Choice(['json', 'marcxml']),
-              default='marcxml',
-              help='Whether to use JSON or MARCXML.')
+              default='marcxml', help='Whether to use JSON or MARCXML.')
 @with_appcontext
-def loadrecords(source, source_type):
+def loadrecords(sources, source_type):
     """Load records migration dump."""
-    click.echo('Loading dump...')
-    data = json.load(source)
-
-    click.echo('Sending tasks to queue...')
-    with click.progressbar(data) as records:
-        for item in records:
-            if current_migrator.records_post_task:
-                chain(
-                    import_record.s(item, source_type=source_type),
-                    current_migrator.records_post_task.s()
-                )()
-            else:
-                import_record.delay(item, source_type=source_type)
+    for idx, source in enumerate(sources, 1):
+        click.echo('Loading dump {0} of {1} ({2})'.format(idx, len(sources),
+                                                          source.name))
+        data = json.load(source)
+        with click.progressbar(data) as records:
+            for item in records:
+                if current_migrator.records_post_task:
+                    chain(
+                        import_record.s(item, source_type=source_type),
+                        current_migrator.records_post_task.s()
+                    )()
+                else:
+                    import_record.delay(item, source_type=source_type)
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), nargs=-1)
 @click.option('--recid', type=int)
 @click.option('--files', 'entity', flag_value='files')
 @click.option('--json', 'entity', flag_value='json')
 @click.option('--marcxml', 'entity', flag_value='marcxml')
 @with_appcontext
-def inspectrecords(source, recid, entity=None):
+def inspectrecords(sources, recid, entity=None):
     """Inspect records in a migration dump."""
-    click.echo('Loading dump...')
-    data = json.load(source)
 
-    # Just print record identifiers if none are selected.
-    if not recid:
-        click.secho('Record identifiers', fg='green')
-        total = 0
-        for r in (d['recid'] for d in data):
-            click.echo(r)
-            total += 1
-        click.echo('{0} records found in dump.'.format(total))
-        return
+    for idx, source in enumerate(sources, 1):
+        click.echo('Loading dump {0} of {1} ({2})'.format(idx, len(sources),
+                                                          source.name))
+        data = json.load(source)
 
-    data = list(filter(lambda d: d['recid'] == recid, data))
+        # Just print record identifiers if none are selected.
+        if not recid:
+            click.secho('Record identifiers', fg='green')
+            total = 0
+            for r in (d['recid'] for d in data):
+                click.echo(r)
+                total += 1
+            click.echo('{0} records found in dump.'.format(total))
+            return
 
-    if not data:
-        click.secho("Record not found.", fg='yellow')
-        return
+        data = list(filter(lambda d: d['recid'] == recid, data))
 
-    for record in data:
-        if entity is None:
-            click.echo(json.dumps(record, indent=2))
-        if entity == 'files':
-            click.secho('Files', fg='green')
-            click.echo(
-                json.dumps(record['files'], indent=2))
+        if not data:
+            click.secho("Record not found.", fg='yellow')
+            return
 
-        if entity == 'json':
-            click.secho('Records (JSON)', fg='green')
-            for revision in record['record']:
-                click.secho(
-                    'Revision {0}'.format(revision['modification_datetime']),
-                    fg='yellow')
-                click.echo(json.dumps(revision['json'], indent=2))
+        for record in data:
+            if entity is None:
+                click.echo(json.dumps(record, indent=2))
+            if entity == 'files':
+                click.secho('Files', fg='green')
+                click.echo(
+                    json.dumps(record['files'], indent=2))
 
-        if entity == 'marcxml':
-            click.secho('Records (MARCXML)', fg='green')
-            for revision in record['record']:
-                click.secho(
-                    'Revision {0}'.format(revision['marcxml']),
-                    fg='yellow')
-                click.echo(revision)
+            if entity == 'json':
+                click.secho('Records (JSON)', fg='green')
+                for revision in record['record']:
+                    click.secho(
+                        'Revision {0}'.format(revision['modification_datetime']),
+                        fg='yellow')
+                    click.echo(json.dumps(revision['json'], indent=2))
+
+            if entity == 'marcxml':
+                click.secho('Records (MARCXML)', fg='green')
+                for revision in record['record']:
+                    click.secho(
+                        'Revision {0}'.format(revision['marcxml']),
+                        fg='yellow')
+                    click.echo(revision)
 
 
-def loadcommon(source, load_task, *args, **kwargs):
+def loadcommon(sources, load_task, *args, **kwargs):
     """Common helper function for load simple objects.
 
     Note: Extra `args` and `kwargs` are passed to the `load_task` function.
 
-    :param source: JSON source file with dumps
-    :type source: str (filepath)
+    :param sources: JSON source files with dumps
+    :type sources: list of str (filepaths)
     :param load_task: Shared task which loads the dump.
     :type load_task: function
     """
-    click.echo('Loading dump...')
-    data = json.load(source)
-    with click.progressbar(data) as data_bar:
-        for d in data_bar:
-            load_task.delay(d, *args, **kwargs)
+    click.echo('Loading dumps started.')
+    for idx, source in enumerate(sources, 1):
+        click.echo('Loading dump {0} of {1} ({2})'.format(idx, len(sources),
+                                                          source.name))
+        data = json.load(source)
+        with click.progressbar(data) as data_bar:
+            for d in data_bar:
+                load_task.delay(d, *args, **kwargs)
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), nargs=-1)
 @click.argument('logos_dir', type=click.Path(exists=True), default=None)
 @with_appcontext
-def loadcommunities(source, logos_dir):
+def loadcommunities(sources, logos_dir):
     """Load communities."""
     from invenio_migrator.tasks.communities import load_community
-    loadcommon(source, load_community, logos_dir)
+    loadcommon(sources, load_community, logos_dir)
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), default=sys.stdin)
 @with_appcontext
-def loadfeatured(source):
+def loadfeatured(sources):
     """Load community featurings."""
     from invenio_migrator.tasks.communities import load_featured
-    loadcommon(source, load_featured)
+    loadcommon(sources, load_featured)
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), nargs=-1)
 @with_appcontext
-def loadusers(source):
+def loadusers(sources):
     """Load users."""
     from .tasks.users import load_user
-    loadcommon(source, load_user)
+    loadcommon(sources, load_user)
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), nargs=-1)
 @with_appcontext
-def loaddeposit(source):
+def loaddeposit(sources):
     """Load deposit."""
     from .tasks.deposit import load_deposit
-    loadcommon(source, load_deposit)
+    loadcommon(sources, load_deposit)
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), nargs=-1)
 @with_appcontext
-def loadremoteaccounts(source):
+def loadremoteaccounts(sources):
     """Load remote accounts."""
     from .tasks.oauthclient import load_remoteaccount
-    loadcommon(source, load_remoteaccount)
+    loadcommon(sources, load_remoteaccount)
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), nargs=-1)
 @with_appcontext
-def loadremotetokens(source):
+def loadremotetokens(sources):
     """Load remote tokens."""
     from .tasks.oauthclient import load_remotetoken
-    loadcommon(source, load_remotetoken)
+    loadcommon(sources, load_remotetoken)
 
 
 @dumps.command()
-@click.argument('source', type=click.File('r'), default=sys.stdin)
+@click.argument('sources', type=click.File('r'), nargs=-1)
 @with_appcontext
-def loaduserexts(source):
+def loaduserexts(sources):
     """Load user identities (legacy UserEXT)."""
     from .tasks.oauthclient import load_userext
-    loadcommon(source, load_userext)
+    loadcommon(sources, load_userext)
